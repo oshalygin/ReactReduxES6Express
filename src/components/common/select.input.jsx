b0154fd1c5bc9baa91bd511625f70e1a6e3e3772
Oleg Shalygin <oshalygin@gmail.com>
2016-07-03 21:16:53 -0700

    added input components and form

--- src/components/common/select.input.jsx
+++ src/components/common/select.input.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { PropTypes } from "react";
 
 const SelectInput = ({name, label, value, defaultOption, options, onChange, error}) => {
-
+    return (
+        <div className="form-group">
+            <div className="field">
+                <select
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    className="form-control">
+                    <option value="">{defaultOption}</option>
+                    {option.map((option) => {
+                        return <option key={option.value} value={option.value}>{option.text}</option>
+                    }) }
+                </select>
+                {error && <div className="alert alert-danger">{error}</div>}
+            </div>
+        </div>
+    );
 };
+
+SelectInput.propTypes = {
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    defaultOption: PropTypes.string,
+    options: PropTypes.arrayOf(PropTypes.object),
+    onChange: PropTypes.func.isRequired,
+    error: PropTypes.string
+
+}
 
 export default SelectInput;
--- src/components/course/course.form.jsx
+++ src/components/course/course.form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { PropTypes } from "react";
 import TextInput from "../common/text.input.jsx";
 import SelectInput from "../common/select.input.jsx";
 
@@ -6,13 +6,41 @@ const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => 
     return (
         <form>
             <h4>Manage Course</h4>
+
             <TextInput
-                name="title"
-                label="Title"
-                value={course.title}
+                name = "title"
+                label = "Title"
+                value = {course.title}
                 onChange = {onChange}
                 error = {errors.title} />
+
+            <SelectInput
+                name = "authorId"
+                label = "Author"
+                value = {course.authorId}
+                defaultOption = "Select Author"
+                options = {allAuthors}
+                onChange = {onChange}
+                error = {errors.authorId} />
+
+            <input
+                type = "submit"
+                disabled = {loading}
+                value = {loading ? "Saving..." : "Save"}
+                className = "btn btn-primary btn-sm"
+                onClick = {onSave} />
 
         </form>
     );
 };
+
+CourseForm.propTypes = {
+    course: PropTypes.object.isRequired,
+    allAuthors: PropTypes.array.isRequired,
+    onSave: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    loading: PropTypes.bool.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+export default CourseForm;
